34baacd7068536ac2227bfb92169693da1719ec4
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-09-18 16:09:28 +0100

    drm: panel: ili9881: Add option to reconfigure setup commands
    The driver is typically asking for LP commands, but then tries
    to send set_display_[on|off] from enable/disable when the host
    will be in HS mode.
    It also sends shutdown commands just before it asserts reset and
    disables the regulator, which is rather redundant.
    
    Add an option to configure these two choices from the panel_desc.
    
    Change-Id: I2d117f6337ecdcffb7673fbda5bd9f2f7c9cc1fd
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -41,12 +41,19 @@ struct ili9881c_instr {
 	} arg;
 };
 
+enum ili9881_desc_flags {
+	ILI9881_FLAGS_NO_SHUTDOWN_CMDS = BIT(0),
+	ILI9881_FLAGS_PANEL_ON_IN_PREPARE = BIT(1),
+	ILI9881_FLAGS_MAX = BIT(31),
+};
+
 struct ili9881c_desc {
 	const struct ili9881c_instr *init;
 	const size_t init_length;
 	const struct drm_display_mode *mode;
 	const unsigned long mode_flags;
 	unsigned int lanes;
+	enum ili9881_desc_flags flags;
 };
 
 struct ili9881c {
@@ -1560,6 +1567,12 @@ static int ili9881c_prepare(struct drm_panel *panel)
 	if (ret)
 		return ret;
 
+	if (ctx->desc->flags & ILI9881_FLAGS_PANEL_ON_IN_PREPARE) {
+		msleep(120);
+
+		ret = mipi_dsi_dcs_set_display_on(ctx->dsi);
+	}
+
 	return 0;
 }
 
@@ -1567,9 +1580,11 @@ static int ili9881c_enable(struct drm_panel *panel)
 {
 	struct ili9881c *ctx = panel_to_ili9881c(panel);
 
-	msleep(120);
+	if (!(ctx->desc->flags & ILI9881_FLAGS_PANEL_ON_IN_PREPARE)) {
+		msleep(120);
 
-	mipi_dsi_dcs_set_display_on(ctx->dsi);
+		mipi_dsi_dcs_set_display_on(ctx->dsi);
+	}
 
 	return 0;
 }
@@ -1578,7 +1593,8 @@ static int ili9881c_disable(struct drm_panel *panel)
 {
 	struct ili9881c *ctx = panel_to_ili9881c(panel);
 
-	mipi_dsi_dcs_set_display_off(ctx->dsi);
+	if (!(ctx->desc->flags & ILI9881_FLAGS_PANEL_ON_IN_PREPARE))
+		mipi_dsi_dcs_set_display_off(ctx->dsi);
 
 	return 0;
 }
@@ -1587,7 +1603,12 @@ static int ili9881c_unprepare(struct drm_panel *panel)
 {
 	struct ili9881c *ctx = panel_to_ili9881c(panel);
 
-	mipi_dsi_dcs_enter_sleep_mode(ctx->dsi);
+	if (!(ctx->desc->flags & ILI9881_FLAGS_NO_SHUTDOWN_CMDS)) {
+		if (ctx->desc->flags & ILI9881_FLAGS_PANEL_ON_IN_PREPARE)
+			mipi_dsi_dcs_set_display_off(ctx->dsi);
+
+		mipi_dsi_dcs_enter_sleep_mode(ctx->dsi);
+	}
 	regulator_disable(ctx->power);
 	gpiod_set_value(ctx->reset, 1);
 
@@ -1873,6 +1894,8 @@ static const struct ili9881c_desc rpi_7inch_desc = {
 	.mode = &rpi_7inch_default_mode,
 	.mode_flags =  MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_LPM,
 	.lanes = 2,
+	.flags = ILI9881_FLAGS_NO_SHUTDOWN_CMDS |
+		 ILI9881_FLAGS_PANEL_ON_IN_PREPARE,
 };
 
 static const struct of_device_id ili9881c_of_match[] = {
