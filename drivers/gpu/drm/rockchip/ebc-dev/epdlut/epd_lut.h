f3564437b608cd68a3392c6bc896c870482cf6c2
Zorro Liu <lyx@rock-chips.com>
2025-05-06 16:45:40 +0800

    drm/rockchip: ebc_dev: release version v7.14
    1.system update no wait framedone under overlay mode
    2.fast mode to normal mode use part glr16 if fast mode not complete
    3.support setting enable waveform fix
    4.support setting force temperature
    
    Change-Id: I63355e094f56f055022bdb6688270cee3f09b705
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- drivers/gpu/drm/rockchip/ebc-dev/epdlut/epd_lut.h
+++ drivers/gpu/drm/rockchip/ebc-dev/epdlut/epd_lut.h
@@ -48,7 +48,7 @@ struct epd_lut_data {
 int epd_lut_from_mem_init(void *waveform);
 int epd_lut_from_file_init(struct device *dev, void *waveform, int size);
 const char *epd_lut_get_wf_version(void);
-int epd_lut_get(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int regal_pix);
+int epd_lut_get(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int wf_fix, int regal_pix);
 int epd_lut_get_original(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic);
 
 //you can change overlay lut mode here
@@ -60,7 +60,7 @@ int epd_overlay_lut(void);
 int pvi_wf_input(void *waveform_file);
 int pvi_wf_add_custom_mode_table(u8 *table, int size);
 const char *pvi_wf_get_version(void);
-int pvi_wf_get_lut(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int regal_pix);
+int pvi_wf_get_lut(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int wf_fix, int regal_pix);
 int pvi_wf_get_original_lut(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic);
 
 /*
@@ -68,5 +68,5 @@ int pvi_wf_get_original_lut(struct epd_lut_data *output, enum epd_lut_type lut_t
  */
 int rkf_wf_input(void *waveform_file);
 const char *rkf_wf_get_version(void);
-int rkf_wf_get_lut(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int regal_pix);
+int rkf_wf_get_lut(struct epd_lut_data *output, enum epd_lut_type lut_type, int temperature, int pic, int wf_fix, int regal_pix);
 #endif
